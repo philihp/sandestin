4a5ee84931ad07ede2fcffb6a880ab59e7b752d8
karenyang <kaiyuany03@gmail.com>
2023-06-17 00:32:52 -0700

    debugged

--- patterns/top_down_white.py
+++ patterns/top_down_white.py
@@ -3,8 +3,6 @@ import argparse
 import numpy as np 
 import sys
 import struct 
-import colorsys
-from matplotlib.colors import hsv_to_rgb
 
 
 parser = argparse.ArgumentParser()
@@ -23,17 +21,11 @@ class Zome:
         self.num_pixels = len(self.pixels)
         self.fps = model_json['framesPerSecond']
         
-    def height(self):
+    def top(self):
         all_z = [self.nodes[i]['point'][2] for i in range(len(self.nodes))]
-        return np.max(all_z) 
+        top = [0.0, 0.0, np.max(all_z)]
+        return top 
 
-    def width_x(self):
-        all_x = [self.nodes[i]['point'][0] for i in range(len(self.nodes))]
-        return np.max(all_x) 
-     
-    def width_y(self):
-        all_y = [self.nodes[i]['point'][1] for i in range(len(self.nodes))]
-        return np.max(all_y) 
 
 def transform_to_byte_str(frame_id: int, rgba_values: list) -> str:
     """Transform the rgba values for all leds in a frame to bytes str for printing. 
@@ -60,38 +52,28 @@ def red_pattern(zome):
         frame_id+=1
 
 def top_down_pattern(zome):
-    total_time_sec = 3 # TODO: can be a variable
+    total_time_sec = 5 # TODO: can be a variable
     total_frames = total_time_sec * zome.fps
-    topZ = zome.height()
-    max_x = zome.width_x()
-    max_y =  zome.width_y()
-    length_factor = 0.7 # TODO: can be a variable 
+    topZ = zome.top()[2]
+
+    length_factor = 0.3 # TODO: can be a variable 
     alpha = 255 #TODO: can be a variable 
     frame_id = 0
-
     while True:
-        timeBias = frame_id / zome.fps
         for _ in range(total_frames):
             rgba_values = np.zeros((zome.num_pixels, 4)).astype(int) #initialize with all zeros 
             thresholdZ = topZ * ( 1 - (frame_id % total_frames) / total_frames) 
             template_color = np.array([255, 255 , 255]) # TODO: can be a variable 
-
-            # base_color = np.array([255, 0 , 0])
-            
             for i, p in enumerate(zome.pixels):
                 if p[2] > thresholdZ:
                     brightness = 1 - (p[2] - thresholdZ) / (topZ * length_factor)
                     if brightness > 0:
-                        hue =  (timeBias + p[0]/max_x +  p[1]/max_y  ) % 1 #hue depends on x
-                        # color = template_color * brightness
-                        color = np.array(colorsys.hsv_to_rgb(hue, brightness, brightness))  * 255
+                        color = template_color * brightness
                         color = color.astype(int)
                         rgba_values[i] = list(color) + [alpha] # combine the RGB, and alpha
                         # print(color,  brightness)
 
                         # sys.stderr.write(rgba_values[i])
-                # else:
-                #     rgba_values[i] =  list(base_color) + [alpha]
             msg = transform_to_byte_str(frame_id, rgba_values)
             sys.stdout.buffer.write(msg)
             frame_id += 1
@@ -104,5 +86,4 @@ if __name__ == "__main__":
     zome = Zome(args.model_file)
     # red_pattern(zome=zome)
     top_down_pattern(zome=zome)
-
 
