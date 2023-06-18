5de0bba898ed75c51c88a6f369eee8d8e20ddec7
karenyang <kaiyuany03@gmail.com>
2023-06-17 21:23:58 -0700

    added some images

--- patterns/img_2d_projection_vertical.py
+++ patterns/img_2d_projection_vertical.py
@@ -15,32 +15,33 @@ args = parser.parse_args()
 def pattern(zome):
     total_time_sec = 10 # TODO: can be a variable
     total_frames = total_time_sec * zome.fps
-    # topZ = zome.height()
-    max_x = zome.width_x()
+    max_z = zome.height()
+    # max_x = zome.width_x()
     max_y = zome.width_y()
     alpha = 255 #TODO: can be a variable 
     frame_id = 0
     img_dir = os.path.dirname(__file__) + '/../media/'
-    X = np.array(zome.pixels)[:,0]
-    Y = np.array(zome.pixels)[:,1]
+    # X = np.array(zome.pixels)[:,0]
+    Y = np.array(zome.pixels)[:,1] # map to image x
+    Z = np.array(zome.pixels)[:,2] # map to image y
     while True: 
         for f in os.listdir(img_dir):
             if not (f.endswith('jpeg') or f.endswith('png') or f.endswith('jpg')):
                 continue
+            if "arrow" not in f:
+                continue
+           
             img_path = os.path.join(img_dir, f)
             img = np.asarray(Image.open(img_path))
             if not (len(img.shape) == 3 and img.shape[2] == 3):
                 sys.stderr.write(f"image file does not have the right shape {img.shape}. File: {f}\n")
                 continue
-            
-            sys.stderr.write(f"file: {f}")
-
             img_dim = img.shape
             img_center = np.array(img.shape)/2.0
             
             for _ in range(total_frames):
                 zoom_factor = min([1, 0.2 + np.sqrt((frame_id % total_frames) / total_frames)]) # how much the img zoom in 
-                rotate_angle = np.sin(frame_id / zome.fps) * 0.5 # how much the img rotate
+                rotate_angle = np.sin(frame_id / zome.fps) * 0.1 # how much the img rotate
                 rotation_matrix = np.array([[np.cos(rotate_angle), -np.sin(rotate_angle)],
                                 [np.sin(rotate_angle), np.cos(rotate_angle)]])
                 rgba_values = np.zeros((zome.num_pixels, 4)).astype(int) #initialize with all zeros 
@@ -49,11 +50,12 @@ def pattern(zome):
                 zoomed_x = int(img_center[0] - zoomed_width / 2)
                 zoomed_y = int(img_center[1] - zoomed_height / 2)
 
-                zoomed_img = img[zoomed_y:zoomed_y + zoomed_height, zoomed_x:zoomed_x + zoomed_width]
+                zoomed_img = img[zoomed_x:zoomed_x + zoomed_width, zoomed_y:zoomed_y + zoomed_height]
                 zoomed_img_dim = zoomed_img.shape 
 
-                ind_X = (X + max_x)/(2*max_x) * zoomed_img_dim[0]
-                ind_Y = (Y + max_y)/(2*max_y) * zoomed_img_dim[1]
+                rotate_z = frame_id/zome.fps * max_y
+                ind_X = ((Y + max_y + rotate_z) % (2*max_y))/(2*max_y) * zoomed_img_dim[0]
+                ind_Y = Z/max_z * zoomed_img_dim[1]
                 rotate_indices = np.dot(rotation_matrix, np.stack([ind_X, ind_Y])).T
                 rotate_indices = rotate_indices.astype(int)
                 rotate_indices[rotate_indices < 0] = 0
@@ -65,6 +67,7 @@ def pattern(zome):
                 
                 alpha_values = np.ones((zome.num_pixels,1)).astype(int) * alpha
                 rgba_values = np.concatenate((color, alpha_values), axis=1)
+                
                 # for i, p in enumerate(zome.pixels):
                 #     ind_x = int(min([zoomed_img_dim[0], (p[0] + max_x)/(2*max_x) * zoomed_img_dim[0]]))
                 #     ind_y = int(min([zoomed_img_dim[1], (p[1] + max_y)/(2*max_y) * zoomed_img_dim[1]]))
@@ -74,6 +77,7 @@ def pattern(zome):
                 #     color = img[new_ind_x, new_ind_y]
                 #     rgba_values[i] = list(color) + [alpha] # combine the RGB, and alpha
                 msg = transform_to_byte_str(frame_id, rgba_values)
+                
                 sys.stdout.buffer.write(msg) # this writes to the stdout which will be read by the app.js to send the frame. 
                 frame_id += 1
 
